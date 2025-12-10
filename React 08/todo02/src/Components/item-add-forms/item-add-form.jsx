200e2be3faaf6f673e553e80b479e6fa65a19ccc
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-12-10 18:27:23 +0400

    08

---
+++ React 08/todo02/src/Components/item-add-forms/item-add-form.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './item-add.css';
+
+export default class ItemAddForm extends Component {
+
+    render() {
+        return (
+            <div className="item-add-form">
+                <button
+                    className="btn btn-outline-secondary"
+                    >
+                    Add Item
+                </button>
+            </div>
+        )
+    }
+}
